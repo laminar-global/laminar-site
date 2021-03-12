b7a561f5b46269d3b723f746f41f111845e5dba2
Yin Shanyang <me@yinshanyang.com>
2021-03-12 18:34:37 +0800

    feature: London address

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 const Footer = () => (
-  <footer className="container mx-auto py-16 px-8 mt-48 mb-8 text-gray-800">
+  <footer className="container px-8 py-16 mx-auto mt-48 mb-8 text-gray-800">
     <div className="flex flex-wrap mt-12 text-left">
-      <div className="flex-none px-4 py-4 w-full sm:w-1/2 lg:w-1/4 text-gray-600">
-        <h2 className="font-display uppercase font text-lg">Headquarters</h2>
+      <div className="flex-none w-full px-4 py-4 text-gray-600 sm:w-1/2 lg:w-1/4">
+        <h2 className="text-lg uppercase font-display font">Headquarters</h2>
+        <p className="mt-1">71-75 Shelton Street</p>
+        <p className="mt-1">Covent Garden</p>
         <p className="mt-1">London</p>
+        <p className="mt-1">WC2H 9JQ</p>
       </div>
-      <div className="flex-none px-4 py-4 w-full sm:w-1/2 lg:w-1/4 text-gray-600">
-        <h2 className="font-display uppercase font text-lg">NOCs</h2>
+      <div className="flex-none w-full px-4 py-4 text-gray-600 sm:w-1/2 lg:w-1/4">
+        <h2 className="text-lg uppercase font-display font">NOCs</h2>
         <p className="mt-1">Toronto | Dubai | Singapore</p>
       </div>
-      <div className="flex-none px-4 py-4 w-full sm:w-1/2 lg:w-1/4 text-gray-600" />
-      <div className="flex-none px-4 py-4 w-full sm:w-1/2 lg:w-1/4 text-gray-600" />
+      <div className="flex-none w-full px-4 py-4 text-gray-600 sm:w-1/2 lg:w-1/4" />
+      <div className="flex-none w-full px-4 py-4 text-gray-600 sm:w-1/2 lg:w-1/4" />
     </div>
   </footer>
 );
