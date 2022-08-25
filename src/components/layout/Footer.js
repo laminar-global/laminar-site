ee463d1b5c98615493a63eb1726f10cb710597e8
anas-abbasi-laminar <111124161+anas-abbasi-laminar@users.noreply.github.com>
2022-08-25 11:45:40 +0530

    v.0.0.2 - Chaupal Case Study - Added

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -10,9 +10,9 @@ const Footer = () => (
         <p className="mt-1">London</p>
         <p className="mt-1">WC2H 9JQ</p>
       </div>
-      <div className="flex-none w-full px-4 py-4 text-gray-600 sm:w-1/2 lg:w-1/4">
+      <div className="flex-none w-full px-4 py-4 text-gray-600 sm:w-1/2 lg:w-1/3">
         <h2 className="text-lg uppercase font-display font">NOCs</h2>
-        <p className="mt-1">Toronto | Dubai | Singapore</p>
+        <p className="mt-1">Toronto | Singapore | Wroclaw | Dubai | Mumbai</p>
       </div>
       <div className="flex-none w-full px-4 py-4 text-gray-600 sm:w-1/2 lg:w-1/4" />
       <div className="flex-none w-full px-4 py-4 text-gray-600 sm:w-1/2 lg:w-1/4" />
